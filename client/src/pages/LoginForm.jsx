74e090548dd225166d1c5596ceaf779238201f81
maglb <magali.lb21@gmail.com>
2024-11-24 15:27:38 -0700

    Nre picture added for homepage

--- client/src/pages/LoginForm.jsx
+++ client/src/pages/LoginForm.jsx
@@ -3,7 +3,7 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import auth from '../utils/auth';
 import { LOGIN_USER } from '../utils/mutations';
-import loginPicture from '../assets/Pictures/login-picture.png';
+import loginPicture from '../assets/Pictures/HomePage.webp';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
@@ -41,54 +41,76 @@ const LoginForm = () => {
 
   return (
     <div className="login-page">
-      <div className="login-form">
-        <h2 className='login-heading'>Login</h2>
-        <Form noValidate validated={validated} onSubmit={handleFormSubmit} className='login-text'>
-          <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-            Something went wrong with your login credentials!
-          </Alert>
+      <div className="login-section-container">
+        <div className="logo-section">
+          <h1>Feastbook</h1>
+          <p className="subhead">A social platform for food enthusiasts</p>
+        </div>
+        <div className="login-form  shadow-sm">
+          <h2 className="login-heading">Login</h2>
+          <Form
+            noValidate
+            validated={validated}
+            onSubmit={handleFormSubmit}
+            className="login-text"
+          >
+            <Alert
+              dismissible
+              onClose={() => setShowAlert(false)}
+              show={showAlert}
+              variant="danger"
+            >
+              Something went wrong with your login credentials!
+            </Alert>
 
-          <Form.Group className='mb-3'>
-            <Form.Label htmlFor='email'>Email</Form.Label>
-            <Form.Control className='form-input login-form-input'
-              type='email'
-              placeholder='Your email'
-              name='email'
-              onChange={handleInputChange}
-              value={userFormData.email}
-              required
-            />
-          </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Control
+                className="form-input login-form-input"
+                type="email"
+                placeholder="Your email"
+                name="email"
+                onChange={handleInputChange}
+                value={userFormData.email}
+                required
+              />
+            </Form.Group>
 
-          <Form.Group className='mb-3'>
-            <Form.Label htmlFor='password'>Password</Form.Label>
-            <Form.Control className='form-input login-form-input'
-              type='password'
-              placeholder='Your password'
-              name='password'
-              onChange={handleInputChange}
-              value={userFormData.password}
-              required
-            />
-          </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="password">Password</Form.Label>
+              <Form.Control
+                className="form-input login-form-input"
+                type="password"
+                placeholder="Your password"
+                name="password"
+                onChange={handleInputChange}
+                value={userFormData.password}
+                required
+              />
+            </Form.Group>
 
-          <Button
-            disabled={!(userFormData.email && userFormData.password)}
-            type='submit'
-            variant='success'>
-            Submit
-          </Button>
-        </Form>
-        <div className="signup-section">
-          <p>Don't have an account? <a href="/signup">Sign up</a></p>
+            <Button
+              disabled={!(userFormData.email && userFormData.password)}
+              type="submit"
+              variant="success"
+            >
+              Submit
+            </Button>
+          </Form>
+          <div className="signup-section">
+            <p>
+              Don't have an account? <a href="/signup">Sign up</a>
+            </p>
+          </div>
         </div>
       </div>
       <div className="right-section">
-        <div className='top-section'>
-          <img src={loginPicture} alt="Descriptive Alt Text" className="login-picture" />
-        </div>
-        <div className="logo-section">
-          <span>Feastbook</span>
+        <div className="top-section">
+          <img
+            src={loginPicture}
+            alt="Descriptive Alt Text"
+            className="login-picture"
+          />
         </div>
       </div>
     </div>
